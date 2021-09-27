a0f7bc09d8720c150cae5946e3d64dd6fdad2467
Vijay Kishan <vijaykishan333@gmail.com>
2021-09-27 14:45:48 +0530

    SavedChanges

---
+++ MiniAssessments/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MiniAssessments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("ID\tName\tClassAndSection");
+            Console.ForegroundColor = ConsoleColor.White;
+            string filename = @"e:\data.txt";
+            if (File.Exists(filename))
+            {
+                string[] lines = File.ReadAllLines(filename);
+                foreach (string s in lines)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
